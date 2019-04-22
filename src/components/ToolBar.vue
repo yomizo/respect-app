45c092fb09ec15d394a0db66612c637bad743b6c
make a splash <macbook@Mac-mini.local>
2019-04-22 16:12:12 +0900

    modify Toolbar color & detail

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,20 +1,15 @@
 <template>
   <v-layout>
-    <v-toolbar app fixed dense clipped-left class="deep-orange">
-      <v-toolbar-title class="headline text-uppercase">
-        <span><a class="white--text" href="#top">{{title}}</a></span>
-        <span class="font-weight-light">{{subtitle}}</span>
-      </v-toolbar-title>
+    <v-toolbar app fixed dense clipped-left class="pink lighten-5">
+      <v-toolbar-items class="text-uppercase">
+        <v-btn flat class="headline grey--text--darken-3 font-weight-bold" href="#top">
+          {{title}}
+        </v-btn>
+      </v-toolbar-items>
       <v-spacer></v-spacer>
-      <v-btn round class="deep-orange--text" @click.stop="doUpdate">
-        <v-avatar size="26">
-          <img src="heart55.png" alt="avatar">
-        </v-avatar>        
-        Respect!
-      </v-btn>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn
-          class="white--text" 
+          class="grey--text--darken-3" 
           v-for="(mn, i) in items" 
           :key="i"  
           :href="mn.href"
@@ -37,8 +32,6 @@
         { title: 'About', icon: 'question_answer', href:'#about' },
         { title: 'Login', icon: 'dashboard', href:'#login' },
         { title: 'Signin', icon: 'question_answer', href:'#signin' },
-        // { title: 'Skills', icon: 'question_answer', href:'#skills' },
-        // { title: 'History', icon: 'question_answer', href:'#history' },
         { title: 'Mypage', icon: 'question_answer', href:'#mypage' },        
       ],
       mini: false,
